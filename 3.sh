b1a3c428e88d424ea2ac14c6296dfe446c5c945f
Trover <74741960+Troversones@users.noreply.github.com>
2023-03-20 02:10:53 +0100

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#if the directory does not exist it prints out Hiba and exit with an error code 11
+if [[ ! -d "konyvtar" ]]; then
+  echo "Hiba"
+  exit 11
+fi
+#checks if the files size is 0 bytes and prints them out without the file extensions
 for file in konyvtar/*
 do
   if [[ -f "$file" && ! -s "$file" ]]; then
